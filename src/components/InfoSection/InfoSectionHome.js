85896eebb6cd2cf0230c1514d5598cc1769083f8
Esteban de la Maza <A01720548@itesm.mx>
2021-06-07 17:48:42 -0500

    added iframe for testing

--- src/components/InfoSection/InfoSectionHome.js
+++ src/components/InfoSection/InfoSectionHome.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Container } from "../../globalStyles";
-import {
-  InfoSec,
-  Img
-} from "./InfoSection.elements";
+import { InfoSec, Img } from "./InfoSection.elements";
+// import game from "../../../game/index.html";
 
-const InfoSection = ({
-  img,
-  alt,
-
-}) => {
+const InfoSection = ({ img, alt }) => {
   return (
     <>
       <InfoSec>
         <Container>
-              <Img src={img} alt={alt} />
+          {/* <Img src={img} alt={alt} /> */}
+          <iframe
+            src="../../../game/index.html"
+            height="600px"
+            width="960px"
+            frameBorder="0"
+          ></iframe>
         </Container>
       </InfoSec>
     </>
